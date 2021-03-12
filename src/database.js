fed9625d1e783ca0917111dad5562204d45dc337
mpolosak <42770518+mpolosak@users.noreply.github.com>
2021-03-12 19:02:25 +0100

    Connect to mongodb

---
+++ src/database.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const mongoClient = require('mongodb').MongoClient
+const mongoUrl = process.env.MONGO_URL
+const dbName = process.env.DB_NAME
+
+let connect = async () => {
+    try{
+        const client = await mongoClient.connect(mongoUrl)
+        const database = client.db(dbName)
+        return database;
+    }
+    catch(_){
+        console.log("Cannot connect to MongoDB. Is MONGO_URL correct?")
+        process.exit(1)
+    }
+}
+
+exports.connect = connect
--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT ?? 3000
 
-app.get('/', (req, res) => {
-    res.json({ greeting: 'Hello world' })
-})
+const main = async () => {
+    const database = await require('./database.js').connect()
 
-app.listen(port, () => {
-    console.log(`Blog backend listening at port ${port}`)
-})
+    app.get('/', (req, res) => {
+        res.json({ greeting: 'Hello world' })
+    })
+
+    app.listen(port, () => {
+        console.log(`Blog backend listening at port ${port}`)
+    })
+}
+
+main()
