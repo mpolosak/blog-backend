1e967f8031007177b010a7a5bc7b0e83e0dc9a73
mpolosak <42770518+mpolosak@users.noreply.github.com>
2021-03-13 12:58:45 +0100

    Graphql api to receive basic info about blog

---
+++ src/blog-info.js
@@ -0,0 +1,16 @@
+class BlogInfo{
+    constructor(database)
+    {
+        const infoCollection = database.collection("blog_info");
+        this.info = infoCollection.findOne({});
+    }
+    async title(){
+        this.info = await this.info;
+        return this.info.title;
+    }
+    async description(){
+        this.info = await this.info;
+        return this.info.description;
+    }
+}
+exports.BlogInfo = BlogInfo
--- src/index.js
+++ src/index.js
@@ -3,12 +3,34 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT ?? 3000
 
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+const { BlogInfo } = require('./blog-info.js')
+
 const main = async () => {
     const database = await require('./database.js').connect()
+    
+    const schema = buildSchema(`
+        type BlogInfo{
+            title: String
+            description: String
+        }
+        type Query {
+            info: BlogInfo 
+        }
+    `);
+    const root = {
+        info: () => {
+            return new BlogInfo(database)
+        }
+    };
 
-    app.get('/', (req, res) => {
-        res.json({ greeting: 'Hello world' })
-    })
+    app.use('/', graphqlHTTP({
+        schema: schema,
+        rootValue: root,
+        graphiql: true,
+    }));
 
     app.listen(port, () => {
         console.log(`Blog backend listening at port ${port}`)
