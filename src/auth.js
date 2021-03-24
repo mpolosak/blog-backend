a9912a624fe54591e78e1a68251ae29a1eae9af0
mpolosak <42770518+mpolosak@users.noreply.github.com>
2021-03-24 12:15:27 +0100

    Hash password before inserting into database

--- src/auth.js
+++ src/auth.js
@@ -1,8 +1,16 @@
-const signUp = async (database, credentials) => {
+const crypto = require('crypto')
+
+const signUp = async (database, {email, password}) => {
     const collection = database.collection('users')
+    const salt = crypto.randomBytes(32)
+    const hashed_password = crypto.scryptSync(password, salt, 64)
     const input = {
         signup_date: new Date(),
-        credentials
+        credentials:{
+            email,
+            salt,
+            password: hashed_password
+        }
     }
     const response = await collection.insertOne(input)
     return response.insertedId
